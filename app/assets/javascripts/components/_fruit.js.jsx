0c0b68ea948df327ebbdb5888f83c7a42859bd50
jhhere <joyicecloud@gmail.com>
2018-09-14 10:49:39 -0700

    Add the edit button

--- app/assets/javascripts/components/_fruit.js.jsx
+++ app/assets/javascripts/components/_fruit.js.jsx
@@ -1,9 +1,17 @@
 class Fruit extends React.Component{
+
+constructor(props){
+	super(props);
+	this.state = {
+		editable: false
+	}
+}
     render(){
         return(
             <div>
               <h1>{this.props.fruit.name}</h1>
               <p>{this.props.fruit.description}</p>
+              <button>{this.state.editable? 'Submit' : 'Edit' }</button>
               <button onClick={() => this.props.handleDelete(this.props.fruit.id)}>Delete</button>
             </div>
         )
