1612fdfa31ca56287687e2fd57e9569c586f9039
jhhere <joyicecloud@gmail.com>
2018-09-14 11:01:14 -0700

    Makes the button edit and submit funcion, but not yet put the changes

--- app/assets/javascripts/components/_fruit.js.jsx
+++ app/assets/javascripts/components/_fruit.js.jsx
@@ -5,13 +5,22 @@ constructor(props){
 	this.state = {
 		editable: false
 	}
+	this.handleEdit = this.handleEdit.bind(this)
+}
+
+handleEdit() {
+	this.setState({
+		editable: !this.state.editable
+	})
 }
     render(){
+			let name = this.state.editable ? <input type='text' ref={input => this.name = input} defaultValue={this.props.fruit.name}/>:<h3>{this.props.fruit.name}</h3>
+    	let description = this.state.editable ? <input type='text' ref={input => this.description = input} defaultValue={this.props.fruit.description}/>:<p>{this.props.fruit.description}</p>
         return(
             <div>
-              <h1>{this.props.fruit.name}</h1>
-              <p>{this.props.fruit.description}</p>
-              <button>{this.state.editable? 'Submit' : 'Edit' }</button>
+              {name}
+              {description}
+              <button onClick={() => this.handleEdit()}>{this.state.editable? 'Submit' : 'Edit' }</button>
               <button onClick={() => this.props.handleDelete(this.props.fruit.id)}>Delete</button>
             </div>
         )
